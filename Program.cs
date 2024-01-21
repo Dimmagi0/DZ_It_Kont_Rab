4d6c7bc5c0d8a944b338b2bf7000aa3a9cf839e3
Dimmagi0 <redscool@mail.ru>
2024-01-21 20:58:15 +0300

    Added metod GetCharacterLength

--- Program.cs
+++ Program.cs
@@ -11,3 +11,15 @@
 string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
 
+void GetCharacterLength(string[] array1, string[] array2)
+{
+    int result = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[result] = array1[i];
+        result++;
+        }
+    }
+}
